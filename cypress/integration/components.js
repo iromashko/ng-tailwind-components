e5d8977d34ce8faac781c6a87a0d3a52cf9f8910
iromashko <iromashko@me.com>
2020-10-05 20:23:49 +0300

    test: accordion

--- cypress/integration/components.js
+++ cypress/integration/components.js
@@ -1,7 +1,17 @@
 /// <reference types="Cypress" />
 
 describe("Accordion Component", () => {
-  it("Show accordion", () => {
+  before(() => {
     cy.visit("");
+  });
+  it("Show accordion", () => {
+    cy.get('[data-cy="accordion-component"]').should("be.visible");
+  });
+  it("Accordion content is hidden", () => {
+    cy.get('[data-cy="accordion-content"]').should("not.be.visible");
+  });
+  it("Open accordion content", () => {
+    cy.get('[data-cy="accordion-title"]').first().click({ force: true });
+    cy.get('[data-cy="accordion-content"]').should("be.visible");
   });
 });
